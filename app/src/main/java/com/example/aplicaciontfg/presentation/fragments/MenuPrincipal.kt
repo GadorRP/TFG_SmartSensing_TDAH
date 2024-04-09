b3f8b8a1c0d88c70e12d61c44029f1d11e401004
GadorRP <romerogador@gmail.com>
2024-04-09 18:44:13 +0200

    servicio funcionando cada 15 segundos

--- app/src/main/java/com/example/aplicaciontfg/presentation/fragments/MenuPrincipal.kt
+++ app/src/main/java/com/example/aplicaciontfg/presentation/fragments/MenuPrincipal.kt
@@ -6,20 +6,28 @@ import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.core.content.ContextCompat
 import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.aplicaciontfg.R
 import com.example.aplicaciontfg.presentation.service.BackServiceSensors
-
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
+import kotlinx.coroutines.launch
 
 
 class MenuPrincipal : Fragment() {
+    //private val ioScope = CoroutineScope(Dispatchers.IO)
 
     @SuppressLint("MissingInflatedId")
     override fun onCreateView(
@@ -42,16 +50,40 @@ class MenuPrincipal : Fragment() {
             findNavController().navigate(R.id.action_menuPrincipal_to_modoCalibracion)
         }
 
+        //startServiceAndRepeat()
 
         // Get scheduler and prepare intent
-        val scheduler = context?.getSystemService(Context.ALARM_SERVICE) as AlarmManager
-        val intent = Intent(context,BackServiceSensors::class.java)
-        val scheduledIntent = PendingIntent.getService(context, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+        //val scheduler = context?.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        //val intent = Intent(context,BackServiceSensors::class.java)
+        //val scheduledIntent = PendingIntent.getService(context, 0, intent, PendingIntent.FLAG_IMMUTABLE)
 
         // Start the service
-        scheduler.setInexactRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), 5000, scheduledIntent)
+        //scheduler.setInexactRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), 5000, scheduledIntent)
+
+        //CON ESTO FNCIONA
+        //Log.d("Servicio cada 5 segundos", "Ejecutando tarea")
+        val context = requireActivity().applicationContext
+        val intent = Intent(context, BackServiceSensors::class.java)
+        intent.putExtra("inputExtra", "Mensaje para el servicio")
+        ContextCompat.startForegroundService(context, intent)
+
         Log.d("EventoTestForeground" , "hola")
         return root
     }
+    /*private fun startServiceAndRepeat() {
+        ioScope.launch {
+            while (isActive) {
+                // Logic to be executed every 5 seconds
+                Log.d("Servicio cada 5 segundos", "Ejecutando tarea")
+                BackServiceSensors.startService(requireContext(), "Mensaje desde el fragmento")
+                delay(15000L) // Wait for 5 seconds
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        ioScope.cancel()
+    }*/
 
 }
--- app/src/main/java/com/example/aplicaciontfg/presentation/service/BackServiceSensor.kt
+++ app/src/main/java/com/example/aplicaciontfg/presentation/service/BackServiceSensor.kt
@@ -1,5 +1,8 @@
 package com.example.aplicaciontfg.presentation.service
 
+import android.app.ForegroundServiceTypeException
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.app.Service
 import android.content.Context
 import android.content.Intent
@@ -7,17 +10,36 @@ import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
+import android.os.Build
 import android.os.IBinder
 import android.os.PowerManager
 import android.util.Log
+import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat
+import com.example.aplicaciontfg.R
 import java.util.Timer
 import java.util.TimerTask
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
 
 
 class BackServiceSensors : Service(), SensorEventListener {
+    private val CHANNEL_ID = "ForegroundService Kotlin"
     private var sensorManager: SensorManager? = null
-    private val timer = Timer()
     private var ultimaLectura: Float? = null
+    private val executorService = Executors.newSingleThreadScheduledExecutor()
+
+    companion object {
+        fun startService(context: Context, message: String) {
+            val startIntent = Intent(context, BackServiceSensors::class.java)
+            startIntent.putExtra("inputExtra", message)
+            ContextCompat.startForegroundService(context, startIntent)
+        }
+        fun stopService(context: Context) {
+            val stopIntent = Intent(context, BackServiceSensors::class.java)
+            context.stopService(stopIntent)
+        }
+    }
 
     override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
         // get sensor manager on starting the service
@@ -29,13 +51,30 @@ class BackServiceSensors : Service(), SensorEventListener {
         val sensor = sensorManager!!.getDefaultSensor(sensorType)
 
 
-        // in this use case since we work with the alarm manager
-        sensorManager!!.registerListener(
-            this, sensor,
-            SensorManager.SENSOR_DELAY_NORMAL
+        executorService.scheduleAtFixedRate(
+            {
+                sensorManager!!.registerListener(
+                    this,
+                    sensor,
+                    SensorManager.SENSOR_DELAY_NORMAL
+                )
+            },
+            0,
+            15000,
+            TimeUnit.MILLISECONDS
         )
 
+        // create notification channel for API 33
+        createNotificationChannel()
+
         Log.d("BackSensorServie" , "Registrado")
+
+        val notification = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setContentTitle("Servicio de sensores en segundo plano")
+            .setContentText("Este servicio está recopilando datos de los sensores.")
+            .setSmallIcon(R.drawable.nervioso)
+            .build()
+        startForeground(1, notification)
 
         return START_STICKY
     }
@@ -47,9 +86,11 @@ class BackServiceSensors : Service(), SensorEventListener {
             ultimaLectura = event.values[0]
 
             sensorManager?.unregisterListener(this)
-            stopSelf()
+            //stopSelf()
+            // Stop the service when the data is collected
+            //stopForeground(STOP_FOREGROUND_REMOVE)
         }
-        Log.d("Eventossssssss" , event.values[0].toString())
+        //Log.d("Eventossssssss" , event.values[0].toString())
 
     }
     override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {
@@ -59,4 +100,12 @@ class BackServiceSensors : Service(), SensorEventListener {
         return null
     }
 
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val serviceChannel = NotificationChannel(CHANNEL_ID, "Foreground Service Channel",
+                NotificationManager.IMPORTANCE_DEFAULT)
+            val manager = getSystemService(NotificationManager::class.java)
+            manager!!.createNotificationChannel(serviceChannel)
+        }
+    }
 }
